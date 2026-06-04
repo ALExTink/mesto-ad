2c480cc77ebd6857ead26d34238902547904201d
ALExTink <132002865+ALExTink@users.noreply.github.com>
2026-06-04 11:31:16 +0500

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite' //Запуск браузера - true, при запуске run dev проект открывался в браузере
+
+export default defineConfig({
+    server: {
+        open: true,
+    },
+});
