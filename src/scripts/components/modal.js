33a69f97a6bdd11f70c94fea9fe6286a6c2cc21b
ALExTink <132002865+ALExTink@users.noreply.github.com>
2026-06-04 11:24:26 +0500

    Update modal.js

--- src/scripts/components/modal.js
+++ src/scripts/components/modal.js
@@ -1,27 +1,27 @@
-const handleEscUp = (evt) => { // Закрытие по Esc
+const handleEscUp = (evt) => { 
   if (evt.key === "Escape") {
     const activePopup = document.querySelector(".popup_is-opened");
     closeModalWindow(activePopup);
   }
 };
 
-export const openModalWindow = (modalWindow) => { // Открыение модального окна
+export const openModalWindow = (modalWindow) => { 
   modalWindow.classList.add("popup_is-opened");
   document.addEventListener("keyup", handleEscUp);
 };
 
-export const closeModalWindow = (modalWindow) => { // Закрытие модального окна
+export const closeModalWindow = (modalWindow) => { 
   modalWindow.classList.remove("popup_is-opened");
   document.removeEventListener("keyup", handleEscUp);
 };
 
-export const setCloseModalWindowEventListeners = (modalWindow) => { // Добавляет обработчики событий при закрытии модального окна
+export const setCloseModalWindowEventListeners = (modalWindow) => { 
   const closeButtonElement = modalWindow.querySelector(".popup__close")
   closeButtonElement.addEventListener("click", () => {
     closeModalWindow(modalWindow);
   });
 
-  modalWindow.addEventListener("mousedown", (evt) => { // Закрытие по клику вне области модального окна
+  modalWindow.addEventListener("mousedown", (evt) => { 
     if (evt.target.classList.contains("popup")) {
       closeModalWindow(modalWindow);
     }
